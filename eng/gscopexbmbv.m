be70906b81b2a09e9af7f6cf31e2097866575fc0
Norton Allen <allen@huarp.harvard.edu>
2020-12-11 14:26:44 -0500

    More granularity in genui

--- eng/gscopexbmbv.m
+++
@@ -1,5 +0,0 @@
-function fig = gscopexbmbv(varargin);
-% gscopexbmbv(...)
-% B3MB 100V1
-ffig = ne_group(varargin,'B3MB 100V1','pscopexbmbvv','pscopexbmbva','pscopexbmbvt','pscopexbmbvs','pscopexbmbvf');
-if nargout > 0 fig = ffig; end
---
+++ eng/gscopexbmbvl.m
@@ -0,0 +1,5 @@
+function fig = gscopexbmbvl(varargin);
+% gscopexbmbvl(...)
+% B3MB 100V1 Load
+ffig = ne_group(varargin,'B3MB 100V1 Load','pscopexbmbvlv','pscopexbmbvla','pscopexbmbvls','pscopexbmbvlf');
+if nargout > 0 fig = ffig; end
