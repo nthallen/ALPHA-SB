b897534bc7a977b6d10f8ff0043c716861a7b5e2
Norton Allen <allen@huarp.harvard.edu>
2022-09-18 19:20:26 -0400

    Post Hang Test extraction updates

---
+++ eng/gscopexmn.m
@@ -0,0 +1,5 @@
+function fig = gscopexmn(varargin);
+% gscopexmn(...)
+% Nav
+ffig = ne_group(varargin,'Nav','pscopexmng','pscopexmnt','pscopexmndthrust','pscopexmntiming','pscopexmns');
+if nargout > 0 fig = ffig; end
