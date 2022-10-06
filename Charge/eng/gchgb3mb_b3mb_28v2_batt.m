80341dc5b190f9248d9acd2f2e7ac3ccccf046a5
Norton Allen <allen@huarp.harvard.edu>
2022-10-06 11:05:19 -0400

    Cleanup git

---
+++ Charge/eng/gchgb3mb_b3mb_28v2_batt.m
@@ -0,0 +1,5 @@
+function fig = gchgb3mb_b3mb_28v2_batt(varargin);
+% gchgb3mb_b3mb_28v2_batt(...)
+% B3MB 28V2 Batt
+ffig = ne_group(varargin,'B3MB 28V2 Batt','pchgb3mb_b3mb_28v2_battv','pchgb3mb_b3mb_28v2_batta','pchgb3mb_b3mb_28v2_battt','pchgb3mb_b3mb_28v2_batts','pchgb3mb_b3mb_28v2_battf');
+if nargout > 0 fig = ffig; end
