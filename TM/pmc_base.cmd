8b9ec7be2d20b9b08da375627808f21dc83b4bca
Norton Allen <allen@huarp.harvard.edu>
2022-11-13 16:19:22 -0500

    pmc_base.cmd: Add gain commands. nav.tma: fix T_acc usage

--- TM/pmc_base.cmd
+++ TM/pmc_base.cmd
@@ -12,6 +12,19 @@
       devID, 15, addr, 1, value ? 1 : 0);
   }
   
+  void PMC_holdreg_16(uint8_t devID, uint16_t addr, uint16_t value) {
+    if_PropMtr.Turf("W%02X:%02X:%04X:%04X:%04X\n",
+      devID, 6, addr, 1, value);
+  }
+  
+  void PMC_holdreg_32(uint8_t devID, uint16_t addr, uint32_t value) {
+    uint16_t data[2];
+    data[0] = value & 0xFFFF;
+    data[1] = (value>>16) & 0xFFFF;
+    if_PropMtr.Turf("W%02X:%02X:%04X:%04X:%04X:%04X\n",
+      devID, 16, addr, 2, data[0], data[1]);
+  }
+  
   void PMC_setpoint(uint8_t devID, uint16_t addr, double setpoint) {
     uint16_t data[2];
     int32_t isp = setpoint * (devID&1 ? 1 : -1);
@@ -22,7 +35,7 @@
   }
   #endif
 %}
- 
+
 &^command
   : PMC &pmc_drive &pmc_state &pmc_enable * {
       PMC_state($2, $3, $4);
@@ -44,6 +57,63 @@
     }
   : PMC &pmc_drive Move to %f (Counts) * {
       PMC_setpoint($2, 321, $5);
+    }
+  : PMC &pmc_drive Set Velocity Loop Proportional Gain %f * {
+      int32_t value;
+      double dvalue;
+      /* dvalue = gain($8) * (2^16 * Vvel * Rppv)/(2*Cpk)
+           Vvel = Switching Freq/2 = 10000
+           Rppv = 1??
+           Cpk = 25A
+       */
+      dvalue = $8 * (32768. * 10000. * 1) / (2*25.);
+      if (dvalue >= 32768 * 16384)
+        dvalue = 32768*16384-1;
+      value = (int32_t)dvalue;
+      PMC_holdreg_32($2, 251, (uint32_t)value);
+    }
+  : PMC &pmc_drive Set Velocity Loop Integral Gain %f * {
+      int32_t value;
+      double dvalue;
+      /* dvalue = gain($8) * (2^32 * Rppv)/(2*Cpk)
+           Rppv = 1??
+           Cpk = 25A
+       */
+      dvalue = $8 < 0 ? 0 : $8 * (32768. * 32768. * 1) / (2*25.);
+      if (dvalue >= 32768 * 16384)
+        dvalue = 32768*16384-1;
+      value = (int32_t)dvalue;
+      PMC_holdreg_32($2, 251, (uint32_t)value);
+    }
+  : PMC &pmc_drive Set Velocity Loop Derivative Gain %f * {
+      int32_t value;
+      double dvalue;
+      /* dvalue = gain($8) * (2^16 * (Vvel^2) * Rppv)/(2*Cpk)
+           Vvel = Switching Freq/2 = 10000
+           Rppv = 1??
+           Cpk = 25A
+       */
+      dvalue = $8 < 0 ? 0 : $8 * (32768. * 1e8 * 1) / (2*25.);
+      if (dvalue >= 32768 * 16384)
+        dvalue = 32768*16384-1;
+      value = (int32_t)dvalue;
+      PMC_holdreg_32($2, 253, (uint32_t)value);
+    }
+  : PMC &pmc_drive Set Torque Current Loop Proportional Gain %f * {
+      int16_t value;
+      double dvalue;
+      /* dvalue = gain($9) * 2^9 */
+      dvalue = $9 >= 0 ? $9 * 512 : 0;
+      value = (int16_t)dvalue;
+      PMC_holdreg_16($2, 46, (uint16_t)value);
+    }
+  : PMC &pmc_drive Set Torque Current Loop Integral Gain %f * {
+      int16_t value;
+      double dvalue;
+      /* dvalue = gain($9) * 2^9 */
+      dvalue = $9 >= 0 ? $9 * 512 : 0;
+      value = (int16_t)dvalue;
+      PMC_holdreg_16($2, 47, (uint16_t)value);
     }
   ;
 
